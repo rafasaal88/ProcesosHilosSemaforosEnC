fc8934a1b99b85ce72c079295443459db041fd10
rafasaal88 <rafasaal88@gmail.com>
2015-06-26 16:10:14 +0200

    Update Ejercicio6.c

--- Hilos/Ejercicio6/Ejercicio6.c
+++ Hilos/Ejercicio6/Ejercicio6.c
@@ -1,125 +1,117 @@
 #include <stdio.h>
-#include <unistd.h>
-#include <pthread.h>
 #include <stdlib.h>
+#include <pthread.h>
 #include <time.h>
 
-struct datos
+struct estructura
 {
-	int escalar;
-	int matriz[3][3];
+	float matriz [3][3];
+	float escalar;
 };
 
-void mostrarmatriz(struct datos dato);
-void *multiplicar(void * dato);
+
+void *multiplica(void *parametro);
 
 
+int main(){
 
-
-int main()
-{
-	struct datos *dato;
-	void *valor;
 	srand(time(NULL));
+
+
+	struct estructura prueba1,prueba2;
+
 	int i=0,j=0;
-	pthread_t thd[2];
-
-	dato=malloc(sizeof(struct datos)*2);
-
-
-
-
-	dato[0].escalar=rand()%10;
-	dato[1].escalar=rand()%10;
-
-	printf("El primer escalar es: %d\n", dato[0].escalar);
-	printf("El segundo escalar es: %d\n", dato[1].escalar);
-
-
-	
-	for(i=0;i<3;i++)
-	{
-		for(j=0;j<3;j++)
+	int nFil=3,nCol=3;
+		prueba1.escalar=rand()%10;
+		prueba2.escalar=rand()%10;
+		for (i=0;i<nFil;i++)
 		{
-			dato[0].matriz[i][j]=rand()%10;
-			dato[1].matriz[i][j]=rand()%10;
+			for (j = 0; j < nCol; j++)
+			{
+				prueba1.matriz[i][j]=rand()%10;
+				prueba2.matriz[i][j]=rand()%10;
+			}
 		}
-	}
-
-	for(i=0;i<2;i++)
-	{
-	mostrarmatriz(dato[i]);	
-	printf("\n");
-	}
-
-	
-
-	for(i=0;i<2;i++)
-	{
-	pthread_create(&thd[i],NULL,(void*)multiplicar,(void*) &dato[i]);
-
-	pthread_join(thd[i],&valor);
-
-
-	
-	dato[i]=*(struct datos*)valor;	
-	}
-
-	for(i=0;i<2;i++)
-	{
-	mostrarmatriz(dato[i]);	
-	printf("\n");
-	}
-
-
-
-
-
-
-return 0;
-}
-
-
-
-
-
-
-
-
-
-void *multiplicar(void* dato)
-{
-
-	struct datos * auxiliar=malloc(sizeof(struct datos));
-
-	auxiliar=(struct datos *)dato;
-
-	int i=0,j=0;
-
-	for(i=0;i<3;i++)
-	{
-		for(j=0;j<3;j++)
+		printf("El escalar es: %f\n", prueba1.escalar);
+		for (i=0;i<nFil;i++)
 		{
-			(*auxiliar).matriz[i][j]=((*auxiliar).matriz[i][j])*((*auxiliar).escalar);
-		}
-
-	}
-
-
-
-	pthread_exit((void*)auxiliar);
-}
-
-void mostrarmatriz(struct datos dato)
-{
-	int i=0,j=0;
-	for(i=0;i<3;i++)
-	{
-		for(j=0;j<3;j++)
-		{
-			printf("%d\t",dato.matriz[i][j]);
+			printf("\n");
+			for (j = 0; j < nCol; j++)
+			{
+				printf("%f\t", prueba1.matriz[i][j]);
+				
+			}
 		}
 		printf("\n");
+		printf("\nEl escalar es: %f\n", prueba2.escalar);
+		for (i=0;i<nFil;i++)
+		{
+			printf("\n");
+			for (j = 0; j < nCol; j++)
+			{
+				printf("%f\t", prueba2.matriz[i][j]);
+				
+			}
+		}
+		printf("\n");
+
+		pthread_t thd1,thd2;
+
+		pthread_create(&thd1,NULL,(void*)multiplica,(void*)&prueba1);
+		pthread_create(&thd2,NULL,(void*)multiplica,(void*)&prueba2);
+
+		pthread_join(thd1,NULL);
+		pthread_join(thd2,NULL);
+
+				
+
+
+		printf("El escalar es: %f\n", prueba1.escalar);
+		for (i=0;i<nFil;i++)
+		{
+			printf("\n");
+			for (j = 0; j < nCol; j++)
+			{
+				printf("%f\t", prueba1.matriz[i][j]);
+				
+			}
+		}
+		printf("\n");
+		printf("\nEl escalar es: %f\n", prueba2.escalar);
+		for (i=0;i<nFil;i++)
+		{
+			printf("\n");
+			for (j = 0; j < nCol; j++)
+			{
+				printf("%f\t", prueba2.matriz[i][j]);
+				
+			}
+		}
+		printf("\n");
+
+
+
+	return 0;
+}
+
+void *multiplica(void *parametro)
+{
+	int i=0, j=0;
+	struct estructura *datos;
+
+	datos=(struct estructura *)parametro;
+
+	int nFil=3,nCol=3;
+
+
+	for (i=0;i<nFil;i++)
+	{
+		for (j = 0; j < nCol; j++)
+		{
+			datos->matriz[i][j]=datos->matriz[i][j]*datos->escalar;
+		}
 	}
 
+
+	pthread_exit(NULL);
 }
