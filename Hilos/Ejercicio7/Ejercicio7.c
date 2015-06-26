490d498870359ed7304be397cd5ef5ca10fc1543
rafasaal88 <rafasaal88@gmail.com>
2015-06-26 17:22:32 +0200

    Update Ejercicio7.c

--- Hilos/Ejercicio7/Ejercicio7.c
+++ Hilos/Ejercicio7/Ejercicio7.c
@@ -1,57 +1,70 @@
 #include <stdio.h>
-#include <pthread.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <pthread.h>
+#include <string.h>
 
 struct estructura
 {
-	
+	char cadena[30];
 	int entero;
-	char cadena[20];
 };
 
-void *incrementa(void* datos);
+void *modificar(void * parametro);
 
 int main()
 {
-	int elementos, i=0;
-	printf("¿Cuantos hilos quiere?: ");
-	scanf("%d", &elementos);
-	void * retorno;
-	struct estructura* datos;
-	datos=malloc(sizeof(struct estructura)*elementos);
+	struct estructura * miestructura;
+	int hebras=0;
+	int i=0;
 
-	pthread_t thd[elementos];
+	printf("¿Cuantas hebras desea crear? ");
+	scanf("%d",&hebras);
 
+	miestructura=(struct estructura *)malloc(sizeof(struct estructura)*hebras);
 
-	for(i=0;i<elementos;i++)
+	for (i=0;i<hebras;i++)
 	{
-		datos[i].entero=5;
-		strcpy((datos[i].cadena),"Esto es una cadena");
+		strcpy((miestructura[i].cadena), "Hola, soy una cadena");
+		miestructura[i].entero=i;
 	}
 
 
-	for(i=0;i<elementos;i++)
+	pthread_t thd[hebras];
+
+	for (i=0;i<hebras;i++)
 	{
-		pthread_create(&thd[i],NULL,(void*) incrementa, (void*) &datos[i]);
-		pthread_join(thd[i],&retorno);
-		datos[i]=*(struct estructura*)retorno;
+		pthread_create(&thd[i],NULL,(void*)modificar, (void*)&miestructura[i]);
 	}
+
+	for (i=0;i<hebras;i++)
+	{
+		pthread_join(thd[i],NULL);
+	}	
+
+	for (i=0;i<hebras;i++)
+	{
+		printf("Estructura %d, la cadena es %s y el entero %d\n", i, miestructura[i].cadena, miestructura[i].entero);
+	}
+
 
 	return 0;
 }
 
-void *incrementa(void* datos)
-{
-
-	struct estructura * auxiliar;
-	auxiliar=malloc(sizeof(struct estructura));
-
-	auxiliar=(struct estructura*)datos;
 
 
-	(*auxiliar).cadena[0]='9';
-	(*auxiliar).entero=((*auxiliar).entero)+1;
+void *modificar(void * parametro){
 
-	pthread_exit((void*)auxiliar);
+
+	struct estructura *aux;
+
+	aux=(struct estructura*)parametro;
+
+	aux->cadena[0]='a';
+	aux->entero=(aux->entero)+1;
+
+
+	pthread_exit(NULL);
 
 }
